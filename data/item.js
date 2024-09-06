399bc9a16cb50ffb5d1fd2ec5bd05c76d368b577
Ankur4600 <tinkushrivastav78@gmail.com>
2024-09-06 19:29:22 +0530

    myntra functioning clone

---
+++ data/item.js
@@ -0,0 +1,148 @@
+const item=[
+    // item1
+    {
+        id:`001`,
+        item_image:`/functioningMyntraClone/images/1.jpg`,
+        rating:{
+            stars:4.5,
+            reviews:1400,
+        },
+        company:`Carlton London`,
+        item_name:`Rhodium-Plated CZ Floral Studs`,
+        price:{
+            current_price:600,
+            original_price:1045,
+            discount:42,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+
+
+    },
+// item 2
+    {
+        id:`002`,
+        item_image:`/functioningMyntraClone/images/2.jpg`,
+        rating:{
+            stars:4.3,
+            reviews:24,
+        },
+        company:`CUKOO`,
+        item_name:`Women Padded Halter Neck Swimming Dress`,
+        price:{
+            current_price:1507,
+            original_price:2599,
+            discount:42,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+    // item 3
+    {
+        id:`003`,
+        item_image:`/functioningMyntraClone/images/3.jpg`,
+        rating:{
+            stars:4.1,
+            reviews:249,
+        },
+        company:`NUEVOSDAMAS`,
+        item_name:`Women Red & White Printed A-Line Knee-Length Skirts `,
+        price:{
+            current_price:495,
+            original_price:1599,
+            discount:69,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+    // item 4
+    {
+        id:`004`,
+        item_image:`/functioningMyntraClone/images/4.jpg`,
+        rating:{
+            stars:5.0,
+            reviews:10,
+        },
+        company:`ADIDAS`,
+        item_name:`Indian Cricket ODI Jersey`,
+        price:{
+            current_price:999,
+            original_price:999,
+            discount:0,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+    // item 5
+    {
+        id:`005`,
+        item_image:`/functioningMyntraClone/images/5.jpg`,
+        rating:{
+            stars:4.2,
+            reviews:3500,
+        },
+        company:`Roadster`,
+        item_name:`Pure Cotton T-shirt`,
+        price:{
+            current_price:489,
+            original_price:1399,
+            discount:65,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+    // item 6
+    {
+        id:`006`,
+        item_image:`/functioningMyntraClone/images/6.jpg`,
+        rating:{
+            stars:0.0,
+            reviews:0,
+        },
+        company:`Nike`,
+        item_name:`Men ReactX Running Shoes`,
+        price:{
+            current_price:14999,
+            original_price:14999,
+            discount:0,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+    // item 7
+    {
+        id:`007`,
+        item_image:`/functioningMyntraClone/images/7.jpg`,
+        rating:{
+            stars:4.2,
+            reviews:388,
+        },
+        company:`The Indian Garage Co`,
+        item_name:`Men Slim Fit Regular Shorts`,
+        price:{
+            current_price:639,
+            original_price:1599,
+            discount:60,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+    // item 8
+    {
+        id:`008`,
+        item_image:`/functioningMyntraClone/images/8.jpg`,
+        rating:{
+            stars:4.2,
+            reviews:5200,
+        },
+        company:`Nivea`,
+        item_name:`Men Fresh Deodrant 150ml`,
+        price:{
+            current_price:142,
+            original_price:185,
+            discount:50,
+        },
+        return_period:14,
+        delivery_date:`10 sep 2014`,
+    },
+]
